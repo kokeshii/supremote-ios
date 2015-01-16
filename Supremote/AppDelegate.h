49975f03b85a552a38d0e804aa28cc9844d4bfe1
kokeshii <darthmincho@gmail.com>
2015-01-16 12:32:30 -0600

    Initial Commit

---
+++ Supremote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Supremote
+//
+//  Created by Lambda Omega on 1/16/15.
+//  Copyright (c) 2015 Supremote. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Supremote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Supremote
+//
+//  Created by Lambda Omega on 1/16/15.
+//  Copyright (c) 2015 Supremote. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
