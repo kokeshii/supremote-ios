054ecf0bdce1b8d9ba4582d9f9215934f6025c4f
kokeshii <darthmincho@gmail.com>
2015-01-22 15:05:45 -0600

    Logout stub, refactor section ids

--- Supremote/SPRemoteListTableViewController.m
+++ Supremote/SPRemoteListTableViewController.m
@@ -10,6 +10,12 @@
 #import "SPRemoteTableViewController.h"
 #import "SPRemoteEntryCell.h"
 
+typedef NS_ENUM(NSInteger, SPRemoteListSection) {
+    SPRemoteListRemoteSection,
+    SPRemoteListActionsSection,
+    SPRemoteListSectionCount
+};
+
 @interface SPRemoteListTableViewController ()
 
 @property (nonatomic, strong) NSArray *remoteList;
@@ -19,21 +25,40 @@
 
 @implementation SPRemoteListTableViewController
 
+
+#pragma mark - View Lifecycle
+
 - (void) viewDidLoad {
     [super viewDidLoad];
 
     
-   [[self signalForRemoteList] subscribeNext:^(id x) {
-       self.remoteList = x;
-       [self.tableView reloadData];
-   } error:^(NSError *error) {
-       [self showAlertWithTitle:@"ERROR" message:@"ERROR"];
-   }];
+    [self loadRemoteList];
     
     
     self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"supremote-logo-small.png"]];
     
 }
+
+-(void) refresh:(UIRefreshControl *)refreshControl {
+    [refreshControl endRefreshing];
+    [self loadRemoteList];
+}
+
+
+#pragma mark - Connection procedures
+
+- (void) loadRemoteList {
+    
+    [[self signalForRemoteList] subscribeNext:^(id x) {
+        self.remoteList = x;
+        [self.tableView reloadData];
+    } error:^(NSError *error) {
+        [self showConnectionUnavailableAlert];
+    }];
+    
+}
+
+#pragma mark - Web Service Signals
 
 - (RACSignal *) signalForRemoteList {
     
@@ -54,28 +79,10 @@
     
 }
 
-- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
 
-- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.remoteList.count;
-}
 
-- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    SPRemoteEntryCell *cell = (SPRemoteEntryCell *)[tableView dequeueReusableCellWithIdentifier:@"SPRemoteEntryCell"];
-    
-    NSDictionary *remote = self.remoteList[indexPath.row];
-    NSDictionary *developer = remote[@"developer"];
-    NSDictionary *authUser = developer[@"auth_user"];
-    
-    cell.remoteNameLabel.text = remote[@"name"];
-    cell.developerNameLabel.text = [NSString stringWithFormat:@"@%@", authUser[@"username"]];
-    
-    return cell;
-    
-}
+
+#pragma mark - Segue
 
 - (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     
@@ -86,16 +93,86 @@
     
 }
 
+#pragma mark - UITableViewDataSource and Delegate
+
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    NSDictionary *remote = self.remoteList[indexPath.row];
+    
+    if (indexPath.section == SPRemoteListRemoteSection) {
+        NSDictionary *remote = self.remoteList[indexPath.row];
+        self.remoteId = remote[@"id"];
+        [self performSegueWithIdentifier:@"SPRemoteSegue" sender:nil];
+    } else {
+        // Logout button pressed
+        NSLog(@"LOGOUT PRESSED");
+    }
+}
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     
-    self.remoteId = remote[@"id"];
-    NSLog(@"REMOTE ID IS: %@", remote[@"id"]);
-    [self performSegueWithIdentifier:@"SPRemoteSegue" sender:nil];
+    if (indexPath.section == SPRemoteListRemoteSection) {
+        
+        SPRemoteEntryCell *cell = (SPRemoteEntryCell *)[tableView dequeueReusableCellWithIdentifier:@"SPRemoteEntryCell"];
+        
+        NSDictionary *remote = self.remoteList[indexPath.row];
+        NSDictionary *developer = remote[@"developer"];
+        NSDictionary *authUser = developer[@"auth_user"];
+        
+        cell.remoteNameLabel.text = remote[@"name"];
+        cell.developerNameLabel.text = [NSString stringWithFormat:@"@%@", authUser[@"username"]];
+        
+        return cell;
+        
+    } else if(indexPath.section == SPRemoteListActionsSection) {
+        
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SPLogoutCell"];
+        
+        return cell;
+        
+    }
+    
+    return nil;
     
 }
+
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    if (section == SPRemoteListRemoteSection) {
+        return self.remoteList.count;
+    } else if(section == SPRemoteListActionsSection) {
+        return 1;
+    }
+    
+    return 0;
+    
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (indexPath.section == SPRemoteListRemoteSection) {
+        return 63.0f;
+    } else  {
+        return 48.0f;
+    }
+    
+}
+
+- (NSString *) tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    
+    if (section == SPRemoteListRemoteSection) {
+        return @"REMOTES";
+    }
+    
+    return nil;
+    
+}
+
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
+    return SPRemoteListSectionCount;
+}
+
 
 @end
